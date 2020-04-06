504e4957e62e8e40410e84f62fd30fc8a30e1311
James Montemagno <james.montemagno@gmail.com>
2020-04-06 14:37:34 -0700

    Add project files.

---
+++ AndroidBackgroundTest/AndroidBackgroundTest/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace AndroidBackgroundTest
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void CheckInUse_Clicked(object sender, EventArgs e)
+        {
+            var result = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+            await DisplayAlert("Status", result.ToString(), "OK");
+        }
+
+        async void RequestInUse_Clicked(object sender, EventArgs e)
+        {
+            var result = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            await DisplayAlert("Status", result.ToString(), "OK");
+        }
+
+        async void CheckAlways_Clicked(object sender, EventArgs e)
+        {
+            var result = await Permissions.CheckStatusAsync<Permissions.LocationAlways>();
+            await DisplayAlert("Status", result.ToString(), "OK");
+
+        }
+
+        async void RequestAlways_Clicked(object sender, EventArgs e)
+        {
+            var result = await Permissions.RequestAsync<Permissions.LocationAlways>();
+            await DisplayAlert("Status", result.ToString(), "OK");
+        }
+    }
+}
